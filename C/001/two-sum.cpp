cf949fac8e4fbb71efb18097f949abc0259da215
TylerZhang0423 <516492597@qq.com>
2018-12-19 11:21:38 +0800

    update

---
+++ C/001/two-sum.cpp
@@ -0,0 +1,18 @@
+
+class Solution{
+public:
+	vector<int> twoSum(vector<int> &nums,int target){
+		int vector_size,i,j;
+		vector<int> result;
+		vector_size=nums.size();
+		for(i=0;i<=vector_size-1;++i){
+			for(j=i+1;j<=vector_size-1;++j){
+				if (nums[i]+nums[j]==target && i!=j){
+					result.push_back(i);
+					result.push_back(j);
+				}
+			}
+		}
+		return result;
+	}
+};
