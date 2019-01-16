5ab17c248eab24fbe937cd711fb47ce3fb719bdb
TylerZhang0423 <516492597@qq.com>
2019-01-16 13:55:16 +0800

    201901161355

---
+++ 832-FlippingAnImage/src/flippinganimage/Solution.java
@@ -0,0 +1,27 @@
+package flippinganimage;
+
+class Solution {
+    public int[][] flipAndInvertImage(int[][] A) {
+       // for (A.length=1)
+    	int [][]B=new int[A.length][A.length];
+    	for (int Arow=0;Arow<A.length;Arow++) {
+    		for (int Acol=0;Acol<A[Arow].length;Acol++) {
+    			B[Arow][Acol]=A[Arow][Acol];
+    		}
+    	}
+    	for (int Arow=0;Arow<A.length;Arow++) {
+    		for (int Acol=0;Acol<A[Arow].length;Acol++) {
+    			A[Arow][Acol]=B[Arow][A[Arow].length-Acol-1];
+    		}
+    	}
+    	for (int Arow=0;Arow<A.length;Arow++) {
+    		for (int Acol=0;Acol<A[Arow].length;Acol++) {
+    			A[Arow][Acol]=1-A[Arow][Acol];
+    			if (A[Arow][Acol]<0)
+    				A[Arow][Acol]=-A[Arow][Acol];
+    		}
+    	}
+    	return A;
+    }
+}
+
