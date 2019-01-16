beb82da13c3110ccacf277aa4fd4edb9d8ea4a4f
TylerZhang0423 <516492597@qq.com>
2019-01-16 22:48:50 +0800

    201901162248

--- 905-SortArrayByParity/src/sortarraybyparity/Solution.java
+++ 905-SortArrayByParity/src/sortarraybyparity/Solution.java
@@ -3,20 +3,18 @@ package sortarraybyparity;
 //给定一个非负整数数组A，返回一个由A的所有偶数元素组成的数组，后面跟A的所有奇数元素。
 public class Solution {
     public int[] sortArrayByParity(int[] A) {
-    	int length=A.length;
-    	List<Integer> C=new ArrayList<>();
-    	int []B=new int[length];
-    	for (int count1=0;count1<B.length;count1++) {
-    		for (int count=count1;count<A.length;count++){
-    			if (A[count]%2==1)
-    				continue;
-    			if (A[count]%2==0) {
-    				B[count1]=A[count];
-    				count1=count1+1;
-    				break;
+    	int i=0;
+    	while(i<3000) {
+    		for (int count=0;count<A.length-1;count++){
+    			if (A[count]%2==1&&A[count+1]%2==0) {
+    				int temp=0;
+    				temp=A[count+1];
+    				A[count+1]=A[count];
+    				A[count]=temp;
     			}
     		}
+    		i++;
     	}
-        return B;
+        return A;
     }
 }
