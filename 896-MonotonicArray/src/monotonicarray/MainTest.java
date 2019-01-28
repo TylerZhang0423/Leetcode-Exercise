9eeabb80f8dad8d78253eddf7e54bc261f1447e2
TylerZhang0423 <516492597@qq.com>
2019-01-28 12:31:49 +0800

    201901281231

---
+++ 896-MonotonicArray/src/monotonicarray/MainTest.java
@@ -0,0 +1,16 @@
+//如果数组是单调递增或单调递减的，那么它是单调的。
+//如果对于所有 i <= j，A[i] <= A[j]，那么数组 A 是单调递增的。 
+//如果对于所有 i <= j，A[i]> = A[j]，那么数组 A 是单调递减的。
+//当给定的数组 A 是单调数组时返回 true，否则返回 false。
+package monotonicarray;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		int[] TestArr= {3,2,1};
+		Solution Test=new Solution();
+		System.out.println(Test.isMonotonic(TestArr));
+	}
+
+}
---
+++ 896-MonotonicArray/src/monotonicarray/Solution.java
@@ -0,0 +1,30 @@
+//如果数组是单调递增或单调递减的，那么它是单调的。
+//如果对于所有 i <= j，A[i] <= A[j]，那么数组 A 是单调递增的。 
+//如果对于所有 i <= j，A[i]> = A[j]，那么数组 A 是单调递减的。
+//当给定的数组 A 是单调数组时返回 true，否则返回 false。
+package monotonicarray;
+
+public class Solution {
+    public boolean isMonotonic(int[] A) {
+    	if (A.length==1)
+    		return true;
+    	int j=0;
+    	while(A[j]==A[j+1]&&j<A.length-2) {
+    	   j++;
+    	}	
+        if (A[j]>A[j+1]) {
+        	for (int i=j+1;i<A.length-1;i++) {
+        		if (A[i]<A[i+1]) {
+        			return false;
+        		}
+        	}
+        }else {
+        	for (int i=j+1;i<A.length-1;i++) {
+        		if (A[i]>A[i+1]) {
+        			return false;
+        		}
+        	} 
+        }
+        return true;
+    }
+}
