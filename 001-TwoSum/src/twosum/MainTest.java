b972dbb86faee66c1871b9be1216d0054fd2d5f0
TylerZhang0423 <516492597@qq.com>
2019-02-03 22:34:08 +0800

    201902032234

---
+++ 001-TwoSum/src/twosum/MainTest.java
@@ -0,0 +1,22 @@
+//1. 两数之和
+//给定一个整数数组 nums 和一个目标值 target，
+//请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+//你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+//示例:
+//给定 nums = [2, 7, 11, 15], target = 9
+//因为 nums[0] + nums[1] = 2 + 7 = 9
+//所以返回 [0, 1]
+package twosum;
+
+public class MainTest {
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		int[] testArrays= {2, 7, 11, 15};
+		int testTarget=9;
+		Solution Test=new Solution();
+		int[] result=Test.twoSum(testArrays, testTarget);
+		for (int k=0;k<result.length;k++) {
+			System.out.print(result[k]+" ");
+		}
+	}
+}
