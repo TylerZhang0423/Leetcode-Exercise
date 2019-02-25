178f51b1173b25a5c0f4b1aa98944f75df988ee3
TylerZhang0423 <516492597@qq.com>
2019-02-25 21:10:32 +0800

    201902252110

---
+++ 028-ImplementstrStr()/src/implementstrStr/Solution.java
@@ -0,0 +1,40 @@
+//28. 实现strStr()
+//实现 strStr() 函数。
+//给定一个 haystack 字符串和一个 needle 字符串，
+//在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。
+//如果不存在，则返回  -1。
+//示例 1:
+//输入: haystack = "hello", needle = "ll"
+//输出: 2
+//示例 2:
+//输入: haystack = "aaaaa", needle = "bba"
+//输出: -1
+//说明:
+//当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+//对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与C语言的 strstr() 以及 Java的 indexOf() 定义相符。
+package implementstrStr;
+
+public class Solution {
+    public int strStr(String haystack, String needle) {
+    	if (needle.length()==0) {
+    		return 0;
+    	}
+    	if (haystack.length()==0||needle.length()>haystack.length()) {
+    		return -1;
+    	}
+        for (int i=0;i<haystack.length();i++) {
+        	for (int j=0;j<needle.length();j++) {
+        		if (i+j>haystack.length()||haystack.charAt(i+j)!=needle.charAt(j)) {
+        			break;
+        		}else {
+        			if (j==needle.length()-1) {
+        				return i;
+        			}else {
+        				continue;
+        			}
+        		}
+        	}
+        }
+        return -1;
+    }
+}
