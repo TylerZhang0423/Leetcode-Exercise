f32720052a277cffedbaf3d3f020ad2b54a39769
TylerZhang0423 <516492597@qq.com>
2019-03-11 15:23:14 +0800

    201903111523

---
+++ 922-SortArrayByParityII/src/sortarraybyparityII/Solution.java
@@ -0,0 +1,31 @@
+//922. 按奇偶排序数组 II
+//给定一个非负整数数组 A， A 中一半整数是奇数，一半整数是偶数。
+//对数组进行排序，以便当 A[i] 为奇数时，i 也是奇数；当 A[i] 为偶数时， i 也是偶数。
+//你可以返回任何满足上述条件的数组作为答案。
+//示例：
+//输入：[4,2,5,7]
+//输出：[4,5,2,7]
+//解释：[4,7,2,5]，[2,5,4,7]，[2,7,4,5] 也会被接受。
+//提示：
+//2 <= A.length <= 20000
+//A.length % 2 == 0
+//0 <= A[i] <= 1000
+package sortarraybyparityII;
+
+public class Solution {
+    public int[] sortArrayByParityII(int[] A) {
+    	int[] res = new int[A.length];
+    	int index1=1;
+    	int index2=0;
+    	for (int i=0;i<A.length;i++) {
+    		if (A[i]%2==0) {
+    			res[index2]=A[i];
+    			index2=index2+2;
+    		}else {
+    			res[index1]=A[i];
+    			index1=index1+2;
+    		}
+    	}
+        return res;
+    }
+}
