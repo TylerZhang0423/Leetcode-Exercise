b8abcdb70982c65f01c95bab166fe5cba055dad1
TylerZhang0423 <516492597@qq.com>
2019-04-04 22:16:16 +0800

    201904042216

--- 110-BalancedBinaryTree/src/balancedbinarytree/Solution.java
+++ 110-BalancedBinaryTree/src/balancedbinarytree/Solution.java
@@ -1,4 +1,4 @@
-/                                                                                                           /110. 平衡二叉树
+//110. 平衡二叉树
 //给定一个二叉树，判断它是否是                                                                                                                                        高度平衡的二叉树。
 //本题中，一棵高度平衡二叉树定义为：
 //一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
@@ -25,21 +25,19 @@ package balancedbinarytree;
 
 public class Solution {
     public boolean isBalanced(TreeNode root) {
-    	//找到递归的终止条件
         if (root==null) {
-        	 
-        	return 
+            return true;
         }
-        //
-        
-        //本级递归要判断当前结点的左右子树是否是平衡的，再分析左右子树的深度差是否小于等于1
-        if (!isBalanced(root.left)||!isBalanced(root.right)) {
-        	
+        if (treeHeight(root.left)-treeHeight(root.right)>=2||treeHeight(root.left)-treeHeight(root.right)<=-2||!isBalanced(root.left)||!isBalanced(root.right)) {
+            return false;
         }
+        return true;
     }
-}
-class result {
-    int leftDepth;
-    int rightDepth;
-    boolean flag;
+    
+    public int treeHeight (TreeNode t) {
+        if (t==null) {
+            return 0;
+        }
+        return Math.max(treeHeight(t.left),treeHeight(t.right))+1;
+    }
 }
