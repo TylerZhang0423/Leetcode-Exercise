7f4f55fc39886ba35afafb8a99f9fcab018f44c8
TylerZhang0423 <516492597@qq.com>
2020-02-26 10:52:05 +0800

    202002261052

---
+++ CodingInterviews/Interview03/src/Solution.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author ：Tyler Zhang
+ * @date ：Created in 2020-02-26 10:19
+ * 面试题03. 数组中重复的数字
+ * 找出数组中重复的数字。
+ * 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。
+ * 数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。
+ * 请找出数组中任意一个重复的数字。
+ *
+ * 示例 1：
+ *
+ * 输入：
+ * [2, 3, 1, 0, 2, 5, 3]
+ * 输出：2 或 3
+ *
+ * 限制：
+ * 2 <= n <= 100000
+ */
+public class Solution {
+    public int findRepeatNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return num;
+            }else {
+                set.add(num);
+            }
+        }
+        return -1;
+    }
+}
